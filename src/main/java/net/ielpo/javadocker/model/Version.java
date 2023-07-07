6c3cc58b4549961849bbce79d33be19a3025471a
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2023-07-07 18:04:43 +0200

    ++

--- src/main/java/net/ielpo/javadocker/model/Version.java
+++ src/main/java/net/ielpo/javadocker/model/Version.java
@@ -1,9 +1,11 @@
 package net.ielpo.javadocker.model;
 
+import java.io.Serializable;
+
 /**
  * @author Alberto Ielpo
  */
-public class Version {
+public class Version implements Serializable {
 
     public String name;
     public String uuid;
